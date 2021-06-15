608134c529d5ff001a1257f7b825a22ffae7ca45
Jan Stamer <jan.stamer@freenet.de>
2021-06-15 08:45:00 +0200

    Fixed intro.

---
+++ samples/03_cats_web/cat_api_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCatsAPIHandler(t *testing.T) {
+	// 1. Create test request
+	req, err := http.NewRequest("GET", "/api/cat", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// 2. Create a ResponseRecorder (which satisfies http.ResponseWriter)
+	recorder := httptest.NewRecorder()
+
+	// 3. Invoke handler
+	catAPIHandler(recorder, req)
+
+	// 4. Check the status code
+	if recorder.Code != http.StatusOK {
+		t.Errorf("Wrong status code: got %v expected %v", recorder.Code, http.StatusOK)
+	}
+}
